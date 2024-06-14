d5c1238eb2c5841e41747b616acf0e9518f201d5
PawanPandey18 <pandeypawan6541@gmail.com>
2024-06-14 09:12:02 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let firstName="";
+let lastName="";
+let dob="";
+let gend="";
+let pass="";
+let conpass="";
+let rese="";
+let reg="";
+
+
+function firstname(a) {
+    firstName=a;
+
+    console.log(a);
+}
+
+function lastname(a) {
+lastName=a;
+    console.log(a);
+}
+function dateofbirth(a) {
+    dob=a;
+
+    console.log(a);
+}
+function GENDSERS(a) {
+    gend=a;
+    console.log(a);
+}
+function Password(a) {
+    pass=a;
+    console.log(a);
+}
+function conformpass(a) {
+    conpass=a;
+    console.log(a);
+}
+function reset(a) {
+    rese=a;
+    console.log(a);
+}
+function register(a) {
+    reg=a;
+    console.log(a);
+    
+}
