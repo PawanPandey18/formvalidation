fd66143cdd9a1d533bab9cd16998c041d949ad7b
PawanPandey18 <pandeypawan6541@gmail.com>
2024-06-16 10:31:10 +0545

    adding validation

--- index.js
+++ index.js
@@ -6,16 +6,23 @@ let pass="";
 let conpass="";
 let rese="";
 let reg="";
+let mail="";
+let firstnameError=document.getElementById("firstnameError");
+let lastnameError=document.getElementById("lastnameError");
+let emailError=document.getElementById("emailError");
+let passwordError=document.getElementById("passwordError");
 
 
 function firstname(a) {
+    firstnameError.innerText="";
+
     firstName=a;
 
-    console.log(a);
+    console.log(firstName);
 }
-
 function lastname(a) {
-lastName=a;
+    lastnameError.innerText="";
+    lastName=a;
     console.log(a);
 }
 function dateofbirth(a) {
@@ -27,7 +34,14 @@ function GENDSERS(a) {
     gend=a;
     console.log(a);
 }
+function Email(a) {
+    emailError.innerText="";
+    mail=a;
+    console.log(a);
+    
+}
 function Password(a) {
+    passwordError.innerText="";
     pass=a;
     console.log(a);
 }
@@ -40,7 +54,32 @@ function reset(a) {
     console.log(a);
 }
 function register(a) {
+
     reg=a;
-    console.log(a);
+    if (firstName.length>0 && mail.length>0 && pass.length>0 && lastName.length>0) {
+         if (pass.length<=8) {
+             passwordError.innerText='Password should not be less than 8 character';
+          }
+          if (pass!==conpass) {
+            passwordError.innerText='password doesnot match'
+          }
+    
+    }
+    else{
+        if (firstName.length===0) {
+           firstnameError.innerText='first name can not be empty';
+        }
+        if (lastName.length===0) {
+            lastnameError.innerText='last name can not be empty';
+         }
+        if (mail.length===0) {
+            emailError.innerText='Email can not be empty';
+         }
+        if (pass.length===0) {
+            passwordError.innerText='password can not be empty';
+         }
+        
+         
+    }
     
 }
