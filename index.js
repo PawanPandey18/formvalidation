b40ae30c675f50fc71430455afbbc30db8da3c25
PawanPandey18 <pandeypawan6541@gmail.com>
2024-07-03 17:01:09 +0545

    hello

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ let conpass="";
 let rese="";
 let reg="";
 let mail="";
+let photos="";
+let data=[];
+let showdata=document.getElementById("show");
 let firstnameError=document.getElementById("firstnameError");
 let lastnameError=document.getElementById("lastnameError");
 let emailError=document.getElementById("emailError");
@@ -53,36 +56,64 @@ function reset(a) {
     rese=a;
     console.log(a);
 }
+function photo(a) {
+    photos=a;
+    console.log(photos);  
+
+}
 function register(a) {
 
-    reg=a;
-    if (firstName.length>0 && mail.length>0 && pass.length>0 && lastName.length>0) {
-         if (pass.length<=8) {
-             passwordError.innerText='Password should not be less than 8 character';
-          }
-        else if(pass!==conpass) {
-            passwordError.innerText='password doesnot match'
-          }
-          else{
+//     reg=a;
+//     if (firstName.length>0 && mail.length>0 && pass.length>0 && lastName.length>0) {
+//          if (pass.length<=8) {
+//              passwordError.innerText='Password should not be less than 8 character';
+//           }
+//         else if(pass!==conpass) {
+//             passwordError.innerText='password doesnot match'
+//           }
+//           else{
 
-        }
+//         }
     
-    }
-    else{
-        if (firstName.length===0) {
-           firstnameError.innerText='first name can not be empty';
-        }
-        if (lastName.length===0) {
-            lastnameError.innerText='last name can not be empty';
-         }
-        if (mail.length===0) {
-            emailError.innerText='Email can not be empty';
-         }
-        if (pass.length===0) {
-            passwordError.innerText='password can not be empty';
-         }
+//     }
+//     else{
+//         if (firstName.length===0) {
+//            firstnameError.innerText='first name can not be empty';
+//         }
+//         if (lastName.length===0) {
+//             lastnameError.innerText='last name can not be empty';
+//          }
+//         if (mail.length===0) {
+//             emailError.innerText='Email can not be empty';
+//          }
+//         if (pass.length===0) {
+//             passwordError.innerText='password can not be empty';
+//          }
         
          
+//     }
+    let variable={
+        firstName:firstName,
+        lastName:lastName,
+        dob:dob,
+        gend:gend,
+        mail:mail,
+        photos:photos
     }
+data.push(variable)
+showData();
     
 }
+function showData() {
+    
+    showdata.innerHTML='';
+    data.map((val,i)=>{
+        showdata.innerHTML+=`<div>${val.firstName}
+        
+        
+        
+        </div>`
+    }
+)
+}
+showData();
